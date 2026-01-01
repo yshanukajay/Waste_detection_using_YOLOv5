ecd1fd8535a874db537c29c058503a099f981b9e
JADY Shanuka <2021ICTS28@vauitc.com>
2026-01-01 18:19:34 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'wasteDetection',
+    version= '0.0.0',
+    author= 'Yohan Shanuka',
+    author_email= 'yshanuka123@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
